da676cf5f22839fc5db88e7d46e314ea22c7ad42
Eliasld <area_causation872@simplelogin.fr>
2025-10-21 20:40:13 +0200

    Update config utility to handle docker behavior + Add Dockerfile for building the backend

--- backend/config/config.go
+++ backend/config/config.go
@@ -2,12 +2,18 @@ package config
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 // Loads environment variables from .env file
 func LoadConfig() {
+	if os.Getenv("DOCKER_ENV") == "true" {
+		log.Println("Running in Docker. Skipping .env file loading...")
+		return
+	}
+
 	if err := godotenv.Load(); err != nil {
 		log.Println("Warning: No .env file found, using system environment variables")
 	}
