caa52905b22dbf18b318a377b2f11eb239d72481
Eliasld <area_causation872@simplelogin.fr>
2025-10-22 20:47:42 +0200

    Update the router to wrap it with the newly created CORS middleware

--- backend/internal/router/router.go
+++ backend/internal/router/router.go
@@ -2,14 +2,16 @@ package router
 
 import (
 	"github.com/EliasLd/Serenite/internal/handlers"
+	"github.com/EliasLd/Serenite/internal/middleware"
 	"net/http"
 )
 
 // Initializes the HTTP router and routes
-func SetupRouter() *http.ServeMux {
+func SetupRouter() http.Handler {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/test", handlers.TestHandler)
 
-	return mux
+	// Wrap the router with CORS middleware
+	return middleware.CORS(mux)
 }
