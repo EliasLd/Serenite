d41602afb451d9833f5a2830ffdf65998c688ef1
Eliasld <area_causation872@simplelogin.fr>
2025-11-15 19:36:17 +0100

    Add base entries handler with placeholder functions

---
+++ backend/internal/handlers/entries.go
@@ -0,0 +1,17 @@
+package handlers
+
+import (
+	"net/http"
+)
+
+func ListEntriesHandler(w http.ResponseWriter, r *http.Request) {
+	//TODO
+}
+
+func CreateEntryHandler(w http.ResponseWriter, r *http.Request) {
+	//TODO
+}
+
+func GetEntryDateHandler(w http.ResponseWriter, r *http.Request) {
+	//TODO
+}
